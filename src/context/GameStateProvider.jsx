9803cd2e58490ac20c85b3f6897710633bd58d0c
bhufnus <99205897+bhufnus@users.noreply.github.com>
2024-05-10 15:08:08 -0500

    handle next round

--- src/context/GameStateProvider.jsx
+++ src/context/GameStateProvider.jsx
@@ -6,8 +6,9 @@ const GameContext = createContext();
 // Provider component to provide game state to its children
 export const GameStateProvider = ({ children }) => {
   const [score, setScore] = useState(0);
-  const [numberOfTiles, setNumberOfTiles] = useState(4);
+  const [numberOfTiles, setNumberOfTiles] = useState(2);
   const [currentSequence, setCurrentSequence] = useState([]);
+  const [sequenceLength, setSequenceLength] = useState(2);
   const [sequenceIndex, setSequenceIndex] = useState(0);
   const [level, setLevel] = useState(1);
 
@@ -22,6 +23,8 @@ export const GameStateProvider = ({ children }) => {
         setNumberOfTiles,
         sequenceIndex,
         setSequenceIndex,
+        sequenceLength,
+        setSequenceLength,
       }}
     >
       {children}
