21fed8bf758c6f1386a591a4f968949bb0368f10
bhufnus <99205897+bhufnus@users.noreply.github.com>
2024-05-10 23:50:31 -0500

    mostly css stuff

--- src/components/PlayBtn.jsx
+++ src/components/PlayBtn.jsx
@@ -2,13 +2,12 @@ import { useGame } from "../context/GameStateProvider";
 import { generateRandomSequence } from "../utils/utils";
 
 export const PlayBtn = () => {
-  const { setCurrentSequence, isGameOver, setIsGameOver } = useGame();
-  
-
+  const { setCurrentSequence, isGameOver, setIsGameOver, setScore } = useGame();
 
   function handlePlay() {
     setCurrentSequence(generateRandomSequence(2, 2));
-    setIsGameOver(false)
+    setScore(0);
+    setIsGameOver(false);
   }
 
   return (
--- src/components/TitleHeader.jsx
+++ src/components/TitleHeader.jsx
@@ -1,28 +1,24 @@
-import React, {useEffect,useState} from 'react'
-import { useGame } from '../context/GameStateProvider';
-
+import React, { useEffect, useState } from "react";
+import { useGame } from "../context/GameStateProvider";
 
 function TitleHeader() {
-    const [title,setTitle] = useState(".mamoryzation")
-    const { isGameOver } = useGame();
+  const [title, setTitle] = useState(".memory");
+  const { isGameOver } = useGame();
 
-    useEffect(() => {
-        changeTitle();
-      }, [isGameOver]); 
+  useEffect(() => {
+    changeTitle();
+  }, [isGameOver]);
 
-    function changeTitle(){
-        if(isGameOver){
-            const gameOver = "GameOver"
-            setTitle(gameOver)
-        } else {
-            const gameOver = ".mamoryzation"
-            setTitle(gameOver)
-        }
-        
+  function changeTitle() {
+    if (isGameOver) {
+      const gameOver = "GameOver";
+      setTitle(gameOver);
+    } else {
+      const gameOver = ".memory";
+      setTitle(gameOver);
     }
-  return (
-    <h1> { title } </h1>
-  )
+  }
+  return <h1> {title} </h1>;
 }
 
-export default TitleHeader
+export default TitleHeader;
