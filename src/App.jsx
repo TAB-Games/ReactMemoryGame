a2f89506de4e25c21d5bb0e80d1b65f5df815939
bhufnus <99205897+bhufnus@users.noreply.github.com>
2024-05-13 12:48:48 -0500

    add env variables

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,38 @@
 import React, { useState, useEffect, useContext } from "react";
-import "./App.css";
+
 import TitleHeader from "./components/TitleHeader";
 
+import "./App.css";
 import GameBoard from "./components/GameBoard";
 import { PlayBtn } from "./components/PlayBtn";
 import Score from "./components/Score";
-import { generateRandomSequence } from "./utils/utils";
 
 import { GameStateProvider } from "./context/GameStateProvider";
-import Leaderboard from "./components/Leaderboard";
 import { UIStateProvider } from "./context/UIStateProvider";
+
+import { initializeApp } from "firebase/app";
+
+const {
+  VITE_API_KEY,
+  VITE_AUTH_DOMAIN,
+  VITE_DB_URL,
+  VITE_PROJECT_ID,
+  VITE_STORAGE_BUCKET,
+  VITE_SENDER_ID,
+  VITE_APP_ID,
+} = import.meta.env;
+
+const firebaseConfig = {
+  apiKey: VITE_API_KEY,
+  authDomain: VITE_AUTH_DOMAIN,
+  databaseURL: VITE_DB_URL,
+  projectId: VITE_PROJECT_ID,
+  storageBucket: VITE_STORAGE_BUCKET,
+  messagingSenderId: VITE_SENDER_ID,
+  appId: VITE_APP_ID,
+};
+
+const app = initializeApp(firebaseConfig);
 
 function App() {
   return (
