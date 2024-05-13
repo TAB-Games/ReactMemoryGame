fada603065c5defdf9252938ed262d35435063fe
bhufnus <99205897+bhufnus@users.noreply.github.com>
2024-05-13 16:30:23 -0500

    remove firebase config from app.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -12,26 +12,6 @@ import { UIStateProvider } from "./context/UIStateProvider";
 
 import { initializeApp } from "firebase/app";
 
-const {
-  VITE_API_KEY,
-  VITE_AUTH_DOMAIN,
-  VITE_DB_URL,
-  VITE_PROJECT_ID,
-  VITE_STORAGE_BUCKET,
-  VITE_SENDER_ID,
-  VITE_APP_ID,
-} = import.meta.env;
-
-const firebaseConfig = {
-  apiKey: VITE_API_KEY,
-  authDomain: VITE_AUTH_DOMAIN,
-  databaseURL: VITE_DB_URL,
-  projectId: VITE_PROJECT_ID,
-  storageBucket: VITE_STORAGE_BUCKET,
-  messagingSenderId: VITE_SENDER_ID,
-  appId: VITE_APP_ID,
-};
-
 const app = initializeApp(firebaseConfig);
 
 function App() {
