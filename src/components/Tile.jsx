d98cf55137985e907f08e631f7b48e3f454d9fd6
bhufnus <99205897+bhufnus@users.noreply.github.com>
2024-05-16 18:50:03 -0500

    sfx :)

--- src/components/Tile.jsx
+++ src/components/Tile.jsx
@@ -1,7 +1,10 @@
 import React, { useContext, useEffect } from "react";
+import { Howl, Howler } from "howler";
 import { useGame } from "../context/GameStateProvider";
 import { generateRandomSequence } from "../utils/utils";
 import { useUI } from "../context/UIStateProvider";
+import correct_12 from "./../assets/audio-sfx/correct_12.mp3";
+import wrong_01 from "./../assets/audio-sfx/wrong_01.mp3";
 
 export const Tile = ({ id, index, handleTileClick, tileColor }) => {
   const {
@@ -18,33 +21,12 @@ export const Tile = ({ id, index, handleTileClick, tileColor }) => {
   } = useGame();
   const { setIsTileFlashing } = useUI();
 
-  // CURRENT GOAL:
-  // Min: 2 + i
-  // Max: numTiles - 2
-
-  //
-
-  function handleNextRound() {
-    const newSequence = generateRandomSequence(
-      sequenceLength + 1,
-      numberOfTiles + 1
-    );
-
-    setNumberOfTiles((prevNumTiles) => prevNumTiles + 1);
-
-    setSequenceLength((prevSeqLength) => prevSeqLength + 1);
-    setCurrentSequence(newSequence);
-    setSequenceIndex(0);
-    setIsTileFlashing(true);
-  }
-
   function incrementSequence() {
     let newSequence;
     let numTiles = numberOfTiles * numberOfTiles;
 
     // end is reached, generate more tiles
     if (sequenceLength >= Math.ceil(numTiles / 2)) {
-      console.log("if", numberOfTiles);
       setSequenceLength(numberOfTiles + 1);
 
       newSequence = generateRandomSequence(
@@ -55,7 +37,6 @@ export const Tile = ({ id, index, handleTileClick, tileColor }) => {
       setNumberOfTiles((prevNumTiles) => prevNumTiles + 1);
       // otherwise increase sequence length only
     } else {
-      console.log("else");
       newSequence = generateRandomSequence(sequenceLength + 1, numberOfTiles);
       setSequenceLength((prevSeqLength) => prevSeqLength + 1);
     }
@@ -66,7 +47,19 @@ export const Tile = ({ id, index, handleTileClick, tileColor }) => {
   }
 
   function handleTileClick() {
+    // TODO: make audio utility to handle these more cleanly
+    const correct_sfx = new Howl({
+      src: [correct_12],
+      volume: 0.1,
+    });
+    const wrong_sfx = new Howl({
+      src: [wrong_01],
+      volume: 0.1,
+    });
+
     if (index === currentSequence[sequenceIndex]) {
+      correct_sfx.play();
+      console.log("play");
       setScore((prevScore) => prevScore + 1);
 
       // if we clicked the last tile in the sequence, go next
@@ -76,6 +69,7 @@ export const Tile = ({ id, index, handleTileClick, tileColor }) => {
         setSequenceIndex((prevIndex) => prevIndex + 1);
       }
     } else {
+      wrong_sfx.play();
       console.log("You clicked tile:", index + 1 + " Wrong!");
       setIsGameOver(true);
     }
